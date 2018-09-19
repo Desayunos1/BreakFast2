6b420cf04964a39e6008aecbcd941554448a2f2f
Miguel Angel Pulido Rodriguez <ingenieromiguelpulido@gmail.com>
2018-09-19 08:06:52 -0500

    Se debe ejecutar comando npm install firebase --save
    se finaliza login.service

--- src/app/Services/login.service.ts
+++ src/app/Services/login.service.ts
@@ -1,9 +1,48 @@
 import { Injectable } from '@angular/core';
+import { Observable } from "rxjs";
+import { AngularFireAuth } from "angularfire2/auth";
+import * as firebase from 'firebase';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
+  private user: Observable<firebase.User>;// se le pone null para evitar que almacene el ultimo
+  private userDetails: firebase.User = null;
 
-  constructor() { }
+  constructor(private _firebaseAuth: AngularFireAuth) {
+    this.user = null;
+    this.user = _firebaseAuth.authState;
+    this.user.subscribe(
+      (user) => {
+        if (user) {
+          this.userDetails = user;
+          //console.log(this.userDetails.uid);
+        }
+        else {
+          this.userDetails = null;
+        }
+      }
+    );
+  }
+
+  signInWithGoogle() {
+    return this._firebaseAuth.auth.signInWithPopup(
+      new firebase.auth.GoogleAuthProvider()
+    )
+  }
+  isLoggedIn() {
+    if(this.userDetails!=null){
+     // if (this.userDetails.uid == null) {
+      if (this.userDetails.uid != "j6EbJDZf3BWagJOekEgNvvZO8pn1") {
+        return false;
+      } else {
+        return true;
+      }
+    }
+  }
+  logOut() {
+    return this._firebaseAuth.auth.signOut();
+  }
 }

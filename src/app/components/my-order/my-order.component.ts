4a96089b09110b12d87e21da27bf7a927e688538
jkvergel <joikaverot@gmail.com>
2018-09-19 15:30:17 -0500

    Creacion del componente de myOrder . -npm install material-design-icons -importa el css en el index <link href="https://fonts.googleapis.com/icon?family=Material+Icons"       rel="stylesheet">

--- src/app/components/my-order/my-order.component.ts
+++ src/app/components/my-order/my-order.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Option } from '../../models/Option';
+import { NgForm, FormGroup,FormControl,Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-my-order',
@@ -6,10 +8,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-order.component.css']
 })
 export class MyOrderComponent implements OnInit {
-
+  TpSnacks: Option[]= [
+    {$key: '1', description: 'Pollo'},
+    {$key: '2', description: 'Carne'}
+  ];
+  ElegirSnackForm : FormGroup;
+  ValorSeleccionado: String='';
+  
   constructor() { }
 
   ngOnInit() {
+    this.ElegirSnackForm = new FormGroup({
+      tipoSnack: new FormControl('',[
+        Validators.required])
+    });
   }
+
+  addPedido(){
+    this.ValorSeleccionado=this.ElegirSnackForm.get('tipoSnack').value;
+  }
+
+  delPedido(){
+    this.ValorSeleccionado='';
+  }
+
+  getErrorMessage(control:string) {
+    if(control=='tipoSnack')
+    return this.ElegirSnackForm.get(control).hasError('required') ? 'Debe elegir el tipo de snack' :'';
+  }
+
+
 
 }

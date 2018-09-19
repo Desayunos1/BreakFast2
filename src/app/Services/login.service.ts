124874792188becf9db9f29386cdafe432da589a
Miguel Angel Pulido Rodriguez <ingenieromiguelpulido@gmail.com>
2018-09-19 17:15:36 -0500

    Se finaliza login

--- src/app/Services/login.service.ts
+++ src/app/Services/login.service.ts
@@ -11,11 +11,12 @@ import { ManageDBService } from './manage-db.service';
 export class LoginService {
   private user: Observable<firebase.User>;// se le pone null para evitar que almacene el ultimo
   private userDetails: firebase.User = null;
-  private subscrition1:Subscription;
-  private subscrition2:Subscription;
+  private subscrition1: Subscription;
+  private subscrition2: Subscription;
   private once: Boolean;
+  public tpUser: number = 2;
 
-  constructor(private _firebaseAuth: AngularFireAuth, public dbService:ManageDBService) {
+  constructor(private _firebaseAuth: AngularFireAuth, public dbService: ManageDBService) {
     this.user = null;
     this.user = _firebaseAuth.authState;
     this.user.subscribe(
@@ -32,64 +33,67 @@ export class LoginService {
   }
 
   signInWithGoogle() {
-    
-    this.once=false;
+
+    this.once = false;
+    this.tpUser = 2;
     return this._firebaseAuth.auth.signInWithPopup(
       new firebase.auth.GoogleAuthProvider()
     )
-    
+
   }
-  verifyUserLogged(){
-    console.log(this.userDetails.uid);
-      console.log(this.userDetails.displayName);
-        console.log(this.userDetails.email);
+  verifyUserLogged() {
+    //rdevuelve un numero 
+    //0: usuario dentro de los no aceptados
+    //1: usuario dentro de los aceptado pendiente por ser aceptado
+    //2: usuario nuevo
+
     this.subscrition1 = this.dbService.getUser(this.userDetails.uid.toString())
-    .snapshotChanges()
-    .subscribe(item=>
-      {
+      .snapshotChanges()
+      .subscribe(item => {
         console.log(item.length);
-        let l1=item.length;
-        this.subscrition2 = this.dbService.getUserAcepted(this.userDetails.uid.toString())
-        .snapshotChanges()
-        .subscribe(item=>
-        {
-          console.log(item.length);          
-          let l2=item.length;
-          if(l1==0){
-            if(l2==0){
-              this.dbService.insertListUser({
-                $key: this.userDetails.uid.toString(),
-                name: this.userDetails.displayName.toString(),
-                email: this.userDetails.email.toString()
-              });
-            }
-
-          }
-        });
+        if (item.length == 0) {
+          this.subscrition2 = this.dbService.getUserAcepted(this.userDetails.uid.toString())
+            .snapshotChanges()
+            .subscribe(item => {
+              console.log(item.length);
+              if (item.length == 0) {
+                this.dbService.insertListUser({
+                  $key: this.userDetails.uid.toString(),
+                  name: this.userDetails.displayName.toString(),
+                  email: this.userDetails.email.toString()
+                });
+                this.tpUser = 2;
+              } else
+                this.tpUser = 1;
+            });
+        }
+        else
+          this.tpUser = 0;
       });
-      
 
-      
+
+
   }
-  ngOnDestroy(){
+  ngOnDestroy() {
     this.subscrition1.unsubscribe();
     this.subscrition2.unsubscribe();
   }
   isLoggedIn() {
-    if(this.userDetails!=null){
+    if (this.userDetails != null) {
       if (this.userDetails.uid == null) {
-      //if (this.userDetails.uid != "j6EbJDZf3BWagJOekEgNvvZO8pn1") {
-      
-        this.once=false;
+        //if (this.userDetails.uid != "j6EbJDZf3BWagJOekEgNvvZO8pn1") {
+
+        this.once = false;
+        this.tpUser = 2;
         return false;
 
       } else {
-        
-        if(!this.once){
-          this.once=true;
+
+        if (!this.once) {
+          this.once = true;
           this.verifyUserLogged();
         }
-        
+
         return true;
       }
     }

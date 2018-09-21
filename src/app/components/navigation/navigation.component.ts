6550245192cc74f907395bf57abcd8a2165590d0
Miguel Angel Pulido Rodriguez <ingenieromiguelpulido@gmail.com>
2018-09-21 10:28:37 -0500

    Se agrega cerrar orden al proceso y se reorganizan tabs

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from '../../Services/login.service';
 import {ThemePalette} from '@angular/material/core';
+import { MatSnackBar, MatTabChangeEvent } from '@angular/material';
+import { ManageDBService } from '../../Services/manage-db.service';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-navigation',
@@ -13,11 +16,32 @@ export class NavigationComponent implements OnInit {
     {name: 'Parce le toca agunatar Hambre hasta que lo acepten !!! Por favor ccomuniquese con el Admin',
     color: 'warn'}
   ];
-  constructor(public authService: LoginService) { }
+  constructor(public authService: LoginService,
+    public snackBar: MatSnackBar,
+    public dbService: ManageDBService) { }
+    selected = new FormControl(0);
 
   ngOnInit() {
   }
+  
+  confirm(event: MatTabChangeEvent){
+    if(parseInt(event.toString())==4){//verifica qu este en el tab cerrar orden
+      //console.log("entro");
+      this.openSnackBar("Esta Seguro Que Desea Finalizar Los Ingresos", "SI");    }
+  }
+openSnackBar(message: string, action: string) {
+    //this.refSnackBar = 
+    this.snackBar.open(message, action, {
+      duration: 5000,
+    }).onAction().subscribe(()=>{
+      this.closeOrders();
+    });
+    //this.refSnackBar
+  }
 
+  closeOrders(){
+    this.dbService.updateListEnableSystem(false);
+  }
 }
 export interface ChipColor {
   name: string;

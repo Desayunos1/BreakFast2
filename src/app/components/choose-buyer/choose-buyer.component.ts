80b05453e12d17e73ccb224813778623f8dafd84
jkvergel <joikaverot@gmail.com>
2018-09-21 16:03:51 -0500

    componente choose buyer

--- src/app/components/choose-buyer/choose-buyer.component.ts
+++ src/app/components/choose-buyer/choose-buyer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/User';
+import {ManageDBService} from "../../Services/manage-db.service";
 
 @Component({
   selector: 'app-choose-buyer',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./choose-buyer.component.css']
 })
 export class ChooseBuyerComponent implements OnInit {
-
-  constructor() { }
+  dataSource: User[]= [];
+  constructor(private manageDB: ManageDBService) { }
+  displayedColumns: string[] = ['Nombre', 'email', 'Elegir'];
 
   ngOnInit() {
+    this.getUsersList();
+  }
+
+  getUsersList(){
+    this.manageDB.getListUserAproved().snapshotChanges()
+    .subscribe(item => {
+      this.dataSource = Array<User>();
+      item.forEach(element => {
+        let x = element.payload.toJSON();
+        x['$key'] = element.key;
+        this.dataSource.push(x as User)
+        console.log(x);
+      })
+    });
   }
 
 }
